a7b7c33b20e26a586a00e3f8a27c2ec8e21d09ce
MoonSuhyeon <anstngus22@naver.com>
2024-08-20 15:39:28 +0900

    수현 커밋

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -5,7 +5,7 @@ const Join = () => {
     <div>
         <h3>Join</h3>
         아이디 : <input type='email' placeholder='aabcd@smhrd.com'></input><br/>
-        비밀번호 : <input type='password' placeholder='aabcd@smhrd.com'></input><br/>
+        비밀번호 : <input type='password' placeholder='영어, 숫자 포함 8자 ~ 16자'></input><br/>
         이름 : <input type='text'></input>
     </div>
   )
