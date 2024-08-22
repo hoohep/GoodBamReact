2804ba5b4f372888222da6f2900049ccbeec8eaa
parksungbum <psb1994@naver.com>
2024-08-22 10:19:58 +0900

    app.js 수정 테스트 연결 페이지 삭제

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home';
 import Redirection from './components/Redirection';
-import LoginSuccess from './components/LoginSuccess';
 import Join from './components/Join';
 import Login from './components/Login';
 import Sleep from './components/Sleep';
@@ -21,7 +20,6 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/join" element={<Join />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path='/loginSuccess' element={<LoginSuccess />}></Route>
                     <Route path='/kakao' element={<Redirection />}></Route>
                     <Route path="/sleep" element={<Sleep />} />
                     <Route path="/list" element={<List />} />
