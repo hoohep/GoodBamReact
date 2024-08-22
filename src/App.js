f16f08a3edf8570be25ee353958a04a389125ce7
jsik0803 <a01090551180@gmail.com>
2024-08-22 11:14:54 +0900

    정식 - App.js 및 Footer, Sleep 수정

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './components/Login';
 import Sleep from './components/Sleep';
 import List from './components/List';
 import Result from './components/Result';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -25,7 +26,7 @@ function App() {
                     <Route path="/list" element={<List />} />
                     <Route path="/result" element={<Result />} />
                 </Routes>
-
+                <Footer />  {/* 모든 페이지에 Footer 표시 */}
             </div>
         </BrowserRouter>
     );
