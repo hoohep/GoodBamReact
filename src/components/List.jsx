51995a578d1056527c86f5c1dfb20cbcda7b68eb
MoonSuhyeon <anstngus22@naver.com>
2024-08-26 16:34:02 +0900

    수현 커밋

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,16 +4,13 @@ import '../style/list.css'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import ListMap from './ListMap';
-import data from '../json/resultList.json';
 
 
 const List = () => {
-    const nav = useNavigate();
-
-    const [sleepList, setSleepList] = useState(null); // 수면 데이터를 저장하는 상태
 
     useAuth()   // 유효성 검사
-    
+    const [sleepList, setSleepList] = useState([]); // 수면 데이터를 저장하는 상태
+
     // 서버에서 데이터 가져오기
     useEffect(() => {
         //토큰 가져오기
@@ -21,39 +18,33 @@ const List = () => {
 
         // API 호출을 통해 수면 데이터 가져오기
         axios.get("http://localhost:8092/resultList", {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-          }
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
         })
-        .then(response => {
-            const data = JSON.stringify(response.data);
-            // console.log('data'+data);
-            
-            // if (!data) {
-            //     setSleepList(null); // 데이터가 없으면 null로 설정
-            // } else {
-            //     setSleepList(data); // 데이터를 상태에 저장
-            // }
-            
-        })
-
-
-    }, [nav])
+            .then(response => {
+                const data = JSON.stringify(response.data);
+                // console.log(data);
+                setSleepList(JSON.parse(data));
+                console.log(sleepList);
+            })
+    }, [])
 
     return (
-        
+
         <div className='list-container'>
 
             <div className='list-title'>
                 Sleep List
             </div>
 
-            <Link to='/result' className='list-link'>
-                {data.list.map((item) => (
-                    <ListMap key={item.no} item={item} />
+            <Link to={'/result'} className='list-link'>
+                {sleepList.map((item) => (
+                    <ListMap key={item.id} item={item} />
                 ))}
             </Link>
+
         </div>
     )
 }
--- src/components/ListMap.jsx
+++ src/components/ListMap.jsx
@@ -6,10 +6,9 @@ const ListMap = ({item}) => {
   return (
     <div>
         <div className='list-content'>
-            <li>{item.date}</li>
+            <li>{item.rdate}</li>
             <li><FaExclamationCircle style={{width:'24px',height:'24px', color:'#333333'}}/></li>
           </div>
-
     </div>
   )
 }
