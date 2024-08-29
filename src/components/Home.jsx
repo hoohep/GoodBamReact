de2816508ee44d1d536e2c3348795654413d1876
MoonSuhyeon <anstngus22@naver.com>
2024-08-29 12:30:50 +0900

    수현 커밋

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -46,10 +46,12 @@ const TextContainer = styled.div`
 `;
 
 const MainText = styled.h1`
-  font-size: 36px;
+  font-size: 40px;
+  font-family: 'BMJUA';
+  font-weight: 500;
 
   @media (max-width: 768px) {
-    font-size: 24px;
+    font-size: 30px;
   }
 `;
 
@@ -130,7 +132,7 @@ const SlideContent = styled.div`
 
 const Home = () => {
     const [sections, setSections] = useState([
-        { id: 1, title: '하루의 마무리를 함께 해요.', content: '잠못드는 밤, 굿밤과 함께라면', img: `${homeImg1}` },
+        { id: 1, title: '하루의 마무리를 함께 해요.', content: '잠 못 드는 밤, 굿밤과 함께라면', img: `${homeImg1}` },
         { id: 2, title: 'Our Service', content: '서비스 장점 소개 내용이 여기에 들어갑니다. 서비스의 특징, 혜택, 사용자 리뷰 등을 포함할 수 있습니다.' },
         { id: 3, title: '추가 탭 1', content: '추가 탭 1의 내용 예문입니다. 관련된 정보나 설명이 여기에 들어갑니다.' },
         { id: 4, title: '추가 탭 2', content: '추가 탭 2의 내용 예문입니다. 관련된 정보나 설명이 여기에 들어갑니다.' },
